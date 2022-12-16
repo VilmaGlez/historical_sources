f07b95ace374d335e18573b76dcd95225fc929d9
Vilma González <56168378+VilmaGlez@users.noreply.github.com>
2022-12-15 23:42:57 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,59 @@
+import re
+from setuptools import setup, find_packages
+import os
+import re
+import codecs
+
+NAME = "historicalsources"
+META_PATH = os.path.join(NAME, "__init__.py")
+REQUIREMENTS = []
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+]
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta), META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+if __name__ == "__main__":
+    setup(
+        name="historicalsources",
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=find_meta("url"),
+        version=find_meta("version"),
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        long_description=open("README.md").read(),
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=["deepl","tensorflow","numpy","matplotlib","collections","pandas","logging","argparse"],
+        classifiers=CLASSIFIERS,
+    )
