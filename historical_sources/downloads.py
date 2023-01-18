87f075b2ed4ae981b05e54d80ffaab38e95b8068
Ignacio Arroyo <iaf@gs.utm.mx>
2023-01-18 01:28:11 -0600

    descargas

--- historical_sources/downloads.py
+++ historical_sources/downloads.py
@@ -3,3 +3,5 @@ import gdown
 def download_results():
     url = "https://drive.google.com/drive/folders/1C8XPmpCFkuHmKT2O4vjhsPSHgBSFkZ4g?usp=share_link"
     gdown.download_folder(url, quiet=True, use_cookies=False)
+
+download_results()
