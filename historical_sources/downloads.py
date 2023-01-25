dc664cd4a4f91ac91f4942b79c5102f98306216f
Ignacio Arroyo <iaf@gs.utm.mx>
2023-01-24 23:52:11 -0600

    changes

--- historical_sources/downloads.py
+++ historical_sources/downloads.py
@@ -1,7 +1,12 @@
+import sysconfig
 import gdown
+import shutil
+
+cwd = str(sysconfig.get_paths()["purelib"]) + '/historical_sources'
 
 def download_results():
     url = "https://drive.google.com/drive/folders/1C8XPmpCFkuHmKT2O4vjhsPSHgBSFkZ4g?usp=share_link"
     gdown.download_folder(url, quiet=True, use_cookies=False)
 
 download_results()
+shutil.move('results_final',cwd)
