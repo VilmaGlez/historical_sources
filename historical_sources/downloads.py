191a8251a9befa86367e0f01e3c46a5d111e27c9
Vilma González <56168378+VilmaGlez@users.noreply.github.com>
2023-01-30 16:58:34 -0600

    Update downloads.py

--- historical_sources/downloads.py
+++ historical_sources/downloads.py
@@ -4,12 +4,16 @@ import shutil
 
 cwd = str(sysconfig.get_paths()["purelib"]) + '/historical_sources'
 
-def download_results():
-    url = "https://drive.google.com/drive/folders/1vPt_QbACi960J8Ocy6iIWfUcd76a7Vrr?usp=share_link"
-    gdown.download_folder(url, quiet=True, use_cookies=False)
-    url1 = "https://drive.google.com/drive/folders/10q0jIksGyZ0PmxWIwkBx4a8X02ufVpVS?usp=share_link"
-    gdown.download_folder(url1, quiet=True, use_cookies=False)
+def download_results(default=True,link=None):
+    if default is True:
+        url = "https://drive.google.com/drive/folders/1vPt_QbACi960J8Ocy6iIWfUcd76a7Vrr?usp=share_link"
+        gdown.download_folder(url, quiet=True, use_cookies=False)
+        url1 = "https://drive.google.com/drive/folders/10q0jIksGyZ0PmxWIwkBx4a8X02ufVpVS?usp=share_link"
+        gdown.download_folder(url1, quiet=True, use_cookies=False)
+        shutil.move('results_final',cwd)
+        shutil.move('datasets',cwd)
+    else:
+        gdown.download_folder(link, quiet=True, use_cookies=False)
 
-download_results()
-shutil.move('results_final',cwd)
-shutil.move('datasets',cwd)
+
+
