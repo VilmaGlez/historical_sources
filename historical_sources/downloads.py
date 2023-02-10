5de5b664190d35423630802f5e27132e54077398
Ignacio Arroyo <iaf@gs.utm.mx>
2023-02-10 00:00:38 -0600

    changes

--- historical_sources/downloads.py
+++ historical_sources/downloads.py
@@ -1,13 +1,18 @@
 import sysconfig
 import gdown
 import shutil
+import os
 
 cwd = str(sysconfig.get_paths()["purelib"]) + '/historical_sources'
-
+nuevaruta = cwd + '/results_final'
 def download_datasets():
     url1 = "https://drive.google.com/drive/folders/1smyfEMsWFzUtmVeRL64Yv5MnFB_hvqk9?usp=share_link"
     gdown.download_folder(url1, quiet=True, use_cookies=False)
     shutil.move('datasets',cwd)
+    try:
+        os.mkdir(nuevaruta)
+    except OSError:
+        print("Created " % nuevaruta)
 
 def download(default=True,link=None):
     if default is True:
